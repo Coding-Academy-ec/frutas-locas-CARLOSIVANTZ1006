f3a0256a6a9675fd7f756a6d75053d0fe39f2667
Tu Nombrewqw <carlosivantorres10@gmail.com>
2024-06-06 11:49:33 -0500

    test

--- ejercicio.py
+++ ejercicio.py
@@ -1,19 +1,13 @@
-# Crea un diccionario llamado "frutas" con las siguientes parejas clave-valor:
-#               "manzana" - "roja"
-#               "banana" - "amarilla"
-#               "pera" - "verde"
-#               "naranja" - "naranja"
-# Imprime el diccionario "frutas" en la consola.
-# Imprime el valor asociado a la clave "banana" en la consola.
-# Imprime el valor asociado a la clave "uva" en la consola.
-
 frutas = {
     "manzana": "roja",
-    "banana": , # banana lleva el valor "amarilla"
+    "banana": "amarilla",
     "pera": "verde",
-    "naranja": # naranja lleva el valor "naranja"
+    "naranja": "naranja"
 }
 
+print("Diccionario de frutas:")
 print(frutas)
-print() # banana lleva el valor "amarilla"
-print(frutas.get("uva"))
+print()
+
+print("Color de la banana:", frutas["banana"])
+print("Color de la uva:", frutas.get("uva"))
